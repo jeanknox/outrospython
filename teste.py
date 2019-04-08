3237b8b92651301d6253bb838d5bf70dbfa0bebe
jean <jeankx@live.com>
2019-04-07 22:24:20 -0300

    07/04

--- teste.py
+++ teste.py
@@ -1,11 +1,11 @@
-"usuarios={jean:00336a9537dc3269125a009b4fd802beda8beccd920e517513577bc162830cee}"
-
+usuarios={jean:00336a9537dc3269125a009b4fd802beda8beccd920e517513577bc162830cee}
+`
 def crypto():
     from Crypto.Hash import SHA256
     c = SHA256.new()
     print c.hexdigest()
 crypto(jean)
-"""while True:
+while True:
     usuario=("Digite um nome de usuario")
     if usuario in usuarios:
         senha=("Digite a senha deste usuario")
@@ -16,6 +16,6 @@ from Crypto.Hash import SHA256
 h = SHA256.new()
 while True:
     chave=str(input(b"Digite o valor a ser codificado:"))
-    h.update(chave)
+
+h.update(chave)
     print h.hexdigest()
-"""
